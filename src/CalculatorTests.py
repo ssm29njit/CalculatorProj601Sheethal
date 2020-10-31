db942b9ab97fa7544b1803baf2c72718be2ae350
BuildTools <unconfigured@null.spigotmc.org>
2020-10-31 00:33:05 -0400

    bugFix - Setup calculator  subtraction method -calling the correct method, used to call add instead of subtract

--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -17,7 +17,7 @@ class MyTestCase(unittest.TestCase):
 
     def test_subtract_method_calculator(self):
         calculator = Calculator()
-        self.assertEqual(calculator.add(2,2), 0)
+        self.assertEqual(calculator.subtract(2,2), 0)
         self.assertEqual(calculator.result, 0)
 
 if __name__ == '__main__':
